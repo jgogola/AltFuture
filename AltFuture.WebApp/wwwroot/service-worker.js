e98893f6e08a0d616cdd8e02dc1c553768b2ac17
jgogola <jason@gogola.us>
2023-11-16 07:30:06 -0500

    2023-11-11-PWA ServiceWorker.JS

--- AltFuture.WebApp/wwwroot/service-worker.js
+++ AltFuture.WebApp/wwwroot/service-worker.js
@@ -2,6 +2,7 @@
 
 // Cache name
 const CACHE_NAME = 'altfuture-cache-v1';
+const MAX_CACHE_AGE = 60 * 1000; 
 const urlsToCache = [
     '/',
     '/css/site.css',
@@ -22,20 +23,40 @@ self.addEventListener('install', event => {
     );
 });
 
-// Cache and return requests
+// Fetch Cache and/or Network response
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                // Cache hit - return response
-                if (response) {
-                    return response;
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(event.request).then(cacheResponse => {
+                // Check if the cache is available and not older than 1 minute
+                if (cacheResponse) {
+                    const fetchTime = cacheResponse.headers.get('sw-fetch-time');
+                    if (fetchTime && (Date.now() - fetchTime) <= 60000) {
+                        console.log('Cache loaded.');
+                        return cacheResponse; // Cache is fresh, return it
+                    }
                 }
-                return fetch(event.request);
-            }
-            )
+
+                // Cache is either old or not present, fetch from network
+                return fetch(event.request.clone()).then(networkResponse => {
+                    // Update the cache with the fresh response
+                    cache.put(event.request, networkResponse.clone());
+
+                    // Add custom header to indicate the fetch time
+                    const newResponse = new Response(networkResponse.body, networkResponse);
+                    newResponse.headers.append('sw-fetch-time', Date.now());
+                    console.log('Network loaded.');
+                    return newResponse;
+                }).catch(() => {
+                    // Network request failed, return cached response
+                    console.log('Cache loaded.');
+                    return cacheResponse;
+                });
+            });
+        })
     );
 });
+
 
 // Update a service worker
 self.addEventListener('activate', event => {
